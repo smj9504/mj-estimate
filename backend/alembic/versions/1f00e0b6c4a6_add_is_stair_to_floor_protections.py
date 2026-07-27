599944b808b8169b8476edfa39d694ff514c2ccb
Minjee Song <minjeesong95@gmail.com>
2026-07-27 14:49:29 +0900

    fix: Alembic migration multiple heads 충돌 해결
    - is_stair migration의 revision ID 중복(a1b2c3d4e5f6) 수정 → 1f00e0b6c4a6
    - down_revision을 None에서 5a3b0301c0a0(현재 merge head)으로 연결
    - 파일명도 새 revision ID에 맞게 변경

--- backend/alembic/versions/1f00e0b6c4a6_add_is_stair_to_floor_protections.py
+++ backend/alembic/versions/1f00e0b6c4a6_add_is_stair_to_floor_protections.py
@@ -1,8 +1,8 @@
 """add is_stair to wm_floor_protections
 
-Revision ID: a1b2c3d4e5f6
-Revises:
-Create Date: 2024-01-01 00:00:00.000000
+Revision ID: 1f00e0b6c4a6
+Revises: 5a3b0301c0a0
+Create Date: 2026-07-27 00:00:00.000000
 
 """
 from alembic import op
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a1b2c3d4e5f6'
-down_revision = None
+revision = '1f00e0b6c4a6'
+down_revision = '5a3b0301c0a0'
 branch_labels = None
 depends_on = None
 
